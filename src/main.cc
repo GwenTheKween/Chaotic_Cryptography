0b1cc0a88df640b8a25be750e7dbf1ea61fc2270
Bruno Larsen <blarsen@redhat.com>
2022-05-21 20:36:15 -0300

    add advanced argument parsing

--- src/main.cc
+++ src/main.cc
@@ -1,17 +1,77 @@
 #include "log_map.h"
 #include <stdlib.h>
 #include <math.h>
+#include <getopt.h>
+#include <string>
+#include <vector>
+
+void usage(const char *prog_name) {
+    std::cerr << "Usage: " << prog_name << "\n";
+
+    std::cerr << "\t-h, --help\t\tPrint this help and exit\n";
+    std::cerr << "\t-s, --seed[=VALUE]\t\tUse VALUE as the starting state for the map.  VALUE must be in (0,1) exclusive.\n";
+    std::cerr << "\t-c, --count[=COUNT]\t\tGenerate COUNT random numbers. Defaults to 1000.\n";
+}
+
+enum OPTION_INDEXES {
+    HELP,
+    SEED,
+    COUNT,
+    OPTION_SIZE
+};
+
+/* Function to parse arguments. Return true if there was a problem.  */
+
+bool parse_arguments(int argc, char** argv,
+                    std::string *args) {
+    struct option longopts[] ={
+        { "help", no_argument, NULL, 'h' },
+        { "seed", required_argument, NULL, 's' },
+        { "count" , required_argument, NULL, 'c' },
+        { 0 }
+    };
+
+    /* Set the defaults here. Empty string means no default.  */
+    args[HELP] = "";
+    args[SEED] = "0.4";
+    args[COUNT] = "1000";
+
+    while(1) {
+        int opt = getopt_long(argc, argv, "hs:c:", longopts, 0);
+
+        /* We finished getting options.  */
+        if(opt == -1) break;
+
+        switch(opt) {
+            case '?':
+            case 'h':
+                usage(argv[0]);
+                return opt == '?';
+            case 's':
+                args[SEED] = optarg;
+                break;
+            case 'c':
+                args[COUNT] = optarg;
+                break;
+        }
+    }
+    return false;
+}
 
 int main(int argc, char** argv){
     double d;
-    if(argc > 1){
-        d = atof (argv[1]);
-    }else{
-        d = 0.4;
+    std::string args[OPTION_SIZE];
+    int count;
+
+    if(parse_arguments(argc, argv, args)){
+        return 1;
     }
+
+    d = atof(args[SEED].c_str());
     d = d - floor(d);
+    count = atoi(args[COUNT].c_str());
     logistic_map m(d);
-    for(int i = 0; i < 100000; i++){
+    for(int i = 0; i < count; i++){
         unsigned char c = m.get_random();
         std::cout << (int) c << '\n';
     }
