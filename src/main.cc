f52e354e48a03dad5ff7c668dcb58a9ab22416be
Bruno Larsen <blarsen@redhat.com>
2022-10-18 09:08:54 +0200

    add sha512 hash use to generate a password

--- src/main.cc
+++ src/main.cc
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 #include <math.h>
 #include <getopt.h>
+#include <string.h>
 #include <string>
 #include <vector>
+
+#include <openssl/sha.h>
 
 void usage(const char *prog_name) {
     std::cerr << "Usage: " << prog_name << "\n";
@@ -56,6 +59,30 @@ bool parse_arguments(int argc, char** argv,
         }
     }
     return false;
+}
+
+/* Function to calculate the SHA512 of the given password, or from the current
+   time, which should only be used to speed up testing, not for actual
+   encrypting.  Not that this library should be used to encrypt anything, but
+   still.
+   
+   Resulting hash will be stored in PWD, which must have 512 bytes.  */
+
+void generate_start(unsigned char* pwd, size_t len){
+    if(len == 0) {
+        /* Generate a random seed and print it out.  */
+        /* TBA DO NOT USE THIS YET.  */
+        memset(pwd,0x00, 512);
+        len = 511;
+    }
+    SHA512_CTX ctx;
+
+    SHA512_Init(&ctx);
+    SHA512_Update(&ctx, pwd, len);
+    SHA512_Final(pwd, &ctx);
+
+    for(size_t i=0; i<len; i++) std::cout<<(int)pwd[i]<<' ';
+    std::cout<<'\n';
 }
 
 int main(int argc, char** argv){
